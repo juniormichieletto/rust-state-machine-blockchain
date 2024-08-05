43ccdadb98ca92a6c285c4cad790ddcda469d233
AJ <juniormichieletto@gmail.com>
2024-08-05 23:59:34 +0100

    Lesson 4: Add balances 1st unit test

--- src/balances.rs
+++ src/balances.rs
@@ -19,3 +19,24 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::balances::Pallet;
+
+    #[test]
+    fn init_balances() {
+        let alice: &String = &"alice".to_string();
+        let bob: &String = &"bob".to_string();
+
+        let mut ballances = Pallet::new();
+
+        assert_eq!(ballances.balance(alice), 0);
+        assert_eq!(ballances.balance(bob), 0);
+
+        ballances.set_balance(alice, 100);
+
+        assert_eq!(ballances.balance(alice), 100);
+        assert_eq!(ballances.balance(bob), 0);
+    }
+}
