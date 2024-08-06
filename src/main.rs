b8cc4ccb4525646d1b63ce6804e77cb3c9cff67a
AJ <juniormichieletto@gmail.com>
2024-08-07 00:17:34 +0100

    2.4: Update main

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,31 @@ impl Runtime {
 }
 
 fn main() {
-    let _ = Runtime::new();
+    let mut runtime = Runtime::new();
+    let alice = "alice".to_string();
+    let bob = "bob".to_string();
+
+    runtime.balances.set_balance(&alice, 100);
+    runtime.system.inc_block_number();
+
+    assert_eq!(runtime.system.block_number(), 1);
+
+    runtime.system.inc_nonce(&alice);
+
+    let _res = runtime
+        .balances
+        .transfer(alice, bob, 30)
+        .map_err(|e| print!("Error to transfer from alice to bob, {}", e));
+
+    runtime.system.inc_nonce(&"alice".to_string());
+    /* TODO: Execute another balance transfer, this time from `alice` to `charlie` for 20. */
+    let _res = runtime
+        .balances
+        .transfer("alice".to_string(), "bob".to_string(), 20)
+        .map_err(|e| print!("Error to transfer from alice to bob, {}", e));
+
+    assert_eq!(runtime.balances.balance(&"alice".to_string()), 50);
+    assert_eq!(runtime.balances.balance(&"bob".to_string()), 50);
+
     println!("Okok");
 }
