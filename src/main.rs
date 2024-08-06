19792727f636558bf1267c1f1c87deee76198d36
AJ <juniormichieletto@gmail.com>
2024-08-06 22:17:51 +0100

    2.3: Create initial Runtime

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 mod balances;
 mod system;
 
-use crate::system::Pallet;
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+    pub system: system::Pallet,
+    pub balances: balances::Pallet,
+}
+
+impl Runtime {
+    fn new() -> Self {
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+    }
+}
 
 fn main() {
-    let _ = Pallet::new();
+    let _ = Runtime::new();
     println!("Okok");
 }
